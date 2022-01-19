b32067443af90c684b1f80d4a762690832cfcf9f
Fede <conoci.federico@gmail.com>
2022-01-19 17:59:00 +0100

    added description to endpoint for swagger

--- src/main/java/com/tui/proof/ws/controller/ClientController.java
+++ src/main/java/com/tui/proof/ws/controller/ClientController.java
@@ -2,6 +2,7 @@ package com.tui.proof.ws.controller;
 
 import com.tui.proof.dto.request.CreateClientRequest;
 import com.tui.proof.service.ClientService;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,6 +23,7 @@ public class ClientController {
     }
 
     @PostMapping("/client/create")
+    @ApiOperation(value = "Create a new client with an address", notes = "All fields are mandatory")
     public ResponseEntity<CreateClientRequest> createCustomer(@Valid @RequestBody CreateClientRequest createClientRequest) {
         return ResponseEntity
                 .created(URI.create("/client/create"))
--- src/main/java/com/tui/proof/ws/controller/PilotesController.java
+++ src/main/java/com/tui/proof/ws/controller/PilotesController.java
@@ -3,6 +3,7 @@ package com.tui.proof.ws.controller;
 import com.tui.proof.dto.request.CreateOrderRequest;
 import com.tui.proof.dto.response.PilotesOrderDtoResponse;
 import com.tui.proof.service.OrderService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,6 +22,7 @@ public class PilotesController {
     }
 
     @PostMapping("/order/pilotes")
+    @ApiOperation(value = "Create a new order for an existing user", notes = "user, address must be provided")
     public ResponseEntity<PilotesOrderDtoResponse> createOrder(@Valid @RequestBody CreateOrderRequest createOrderRequest) {
         return ResponseEntity
                 .created(URI.create("/order/pilotes"))
